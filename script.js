5d3e8adda0ceb5b6922495d323dd76330b7554bf
Marta Bobowska <martabobowska2@interia.pl>
2018-08-15 17:09:15 +0200

    Added extended logs

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 var Counter = React.createClass({
 	getDefaultProps: function() {
-		console.log('getDefaultProps');
+		console.log('getDefaultProps - definiowanie domyślnych propsów dostępnych poprzez this.props');
 	},
 
 	getInitialState: function() {
-		console.log('getInitialState');
+		console.log('getInitialState - definiowanie prywatnych danych o obiekcie dostępnych z this.state');
 		return {
 			counter: 0
 		};
 	},
 
 	componentWillMount: function() {
-		console.log('componentWillMount');
+		console.log('componentWillMount - ostatni czas na optymalizacje przed wywołaniem this.render');
+		console.log('componentWillMount - aktualizowanie stanu poprzez this.setState (jednak nie będzie to miało wpływu na this.render)');
 	},
 
 	componentDidMount: function() {
-		console.log('componentDidMount');
+		console.log('componentDidMount - jest to etap, na którym można inicjalizować pobieranie danych z serwerów (np. AJAX)');
 	},
 
 	componentWillReceiveProps: function(nextProps) {
-		console.log('componentWillReceiveProps');
+		console.log('componentWillReceiveProps - przykład: można ustawiać wewnętrzny stan objektu poprzez w zależoności od nowych propsów');
 	},
 
 	shouldComponentUpdate: function(nextProps, nextState) {
-		console.log('shouldComponentUpdate');
+		console.log('shouldComponentUpdate - tu podejmujemy decyzję o konieczności aktualizowania wyświetlanych obiektów poprzez this.render');
 		return true;
 	},
 
 	componentWillUpdate: function(nextProps, nextState) {
-		console.log('componentWillUpdate');
+		console.log('componentWillUpdate - przeznaczenie jest analogiczne to componentWillMount jednak w procesie aktualizacji');
 	},
 
 	componentDidUpdate: function(nextProps, nextState) {
-		console.log('componentDidUpdate');
+		console.log('componentDidUpdate - odpowiednik componentDidMount jednak w procesie aktualizacji');
 	},
 
 	componentWillUnmount: function() {
-		console.log('componentWillUnmount');
+		console.log('componentWillUnmount - wykonywanie zadań sprzątających i czyszczących');
 	},
 
 	increment: function() {
