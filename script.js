5f9161b44bb16c641a28465c1440af6f3dc5d572
Marta Bobowska <martabobowska2@interia.pl>
2018-08-15 16:26:59 +0200

    Added increment and decrement logic

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 var Counter = React.createClass({
+	getInitialState: function() {
+		return {
+			counter: 0
+		};
+	},
+
+	increment: function() {
+		this.setState({
+			counter: this.state.counter + 1
+		});
+	},
+
+
+	decrement: function() {
+		this.setState({
+			counter: this.state.counter - 1
+		});
+	},
+
 	render: function() {
 		return React.createElement('div', {},
-					React.createElement('button', {}, '+'),
-					React.createElement('button', {}, '-'),
-					React.createElement('span', {}, 'Counter value: 0'),
+					React.createElement('button', {onClick: this.increment}, '+'),
+					React.createElement('button', {onClick: this.decrement}, '-'),
+					React.createElement('span', {}, 'Counter value: ' + this.state.counter),
 				);
 	}
 });
